4dc041f5b7734931f2a05b7b6e2f2a71a25da002
prabhuram98 <prabhuram9841@gmail.com>
2026-08-15 17:05:02 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "prabhu-bus-v1";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(APP_FILES))
+
+    );
+
+});
+
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+
+            );
+
+        })
+
+    );
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        fetch(event.request)
+            .catch(() => caches.match(event.request))
+
+    );
+
+});
